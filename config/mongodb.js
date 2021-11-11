c869ea312d9fe2e5666466940ac8b3fc8b4b0358
rajasyah <rajasyahacp20@gmail.com>
2021-11-12 00:11:48 +0700

    Connecting mongodb

---
+++ config/mongodb.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+const url =
+  "mongodb://rajasyah:rajasyah20032003@localhost:27017?authSource=eduwork";
+const client = new MongoClient(url);
+
+(async () => {
+  try {
+    await client.connect();
+    console.log("Connecting to mongodb was successfully");
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
+const db = client.db("eduwork");
+
+module.exports = db;
